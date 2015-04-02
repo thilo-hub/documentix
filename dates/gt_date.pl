8f874bbdda324ff05a6931cbbeb721bf2bb32f7e
Thilo Jeremias <thilo@nispuk.com>
2015-04-02 10:02:15 +0200

    Start date processing

---
+++ dates/gt_date.pl
@@ -0,0 +1,18 @@
+use datematch;
+
+open(UNUSED,">unused.out");
+my $t;
+{ local $/; $t=<>;}
+my $i=undef;
+do {
+	my ($un,$tm,$m,$l)=datematch::extr_date($t);
+	if($tm)
+	{
+		print "$tm\t>$m<\n";
+		$l =~ s/$m//gs;
+	}
+	$t=$l;
+	$i .= $un;
+} while ($t);
+#print UNUSED $i;
+print $i;
