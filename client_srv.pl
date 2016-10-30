0733f83bbc060def69c70160e0d2580cc2a8ed8a
Thilo Jeremias <thilo@nispuk.com>
2016-10-30 17:46:16 +0100

    Client only implementation -- some stuff works

---
+++ client_srv.pl
@@ -0,0 +1,185 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+$ENV{"DISABLE_AUTH"}=1;
+
+use CGI qw/ :standard /;
+use Data::Dumper;
+use HTTP::Daemon;
+use HTTP::Response;
+use HTTP::Status;
+use POSIX qw/ WNOHANG /;
+use ld_r; 
+
+use constant HOSTNAME => qx{hostname};
+
+my %O = (
+    'listen-host' => '192.168.0.11',
+    # 'listen-host' => '127.0.0.1',
+    'listen-port' => 8080,
+    'listen-clients' => 30,
+    'listen-max-req-per-child' => 100,
+);
+
+my $d = HTTP::Daemon->new(
+    LocalAddr => $O{'listen-host'},
+    LocalPort => $O{'listen-port'},
+    Reuse => 1,
+) or die "Can't start http listener at $O{'listen-host'}:$O{'listen-port'}";
+
+print "Started HTTP listener at " . $d->url . "\n";
+
+# system("open http://localhost:8080/res/index.html");
+my %chld;
+
+if ($O{'listen-clients'}) {
+    $SIG{CHLD} = sub {
+        # checkout finished children
+        while ((my $kid = waitpid(-1, WNOHANG)) > 0) {
+            delete $chld{$kid};
+        }
+    };
+}
+
+my @pages= get_pg();
+while (1) {
+    if ($O{'listen-clients'}) {
+        # prefork all at once
+        for (scalar(keys %chld) .. $O{'listen-clients'} - 1 ) {
+            my $pid = fork;
+
+            if (!defined $pid) { # error
+                die "Can't fork for http child $_: $!";
+            }
+            if ($pid) { # parent
+                $chld{$pid} = 1;
+            }
+            else { # child
+                $_ = 'DEFAULT' for @SIG{qw/ INT TERM CHLD /};
+                http_child($d);
+                exit;
+            }
+        }
+
+        sleep 1;
+    }
+    else {
+        http_child($d);
+    }
+
+}
+
+sub http_child {
+    my $d = shift;
+
+    my $i;
+    my $css = <<CSS;
+        form { display: inline; }
+CSS
+
+    while (++$i < $O{'listen-max-req-per-child'}) {
+        my $c = $d->accept or last;
+        my $r = $c->get_request() or last;
+        $c->autoflush(1);
+
+        print sprintf("[%s] %s %s\n", $c->peerhost, $r->method, $r->uri->as_string);
+
+	my $matches=0;
+	# Get args POST/GET -- I assume all ARGS are SHORT
+	    my $arg;
+	    foreach( split(/&/, $r->content) )
+		{  my ($k,$v)=split(/=/,$_,2);
+		   $arg->{$k}=$v;
+		}
+	    if ( $r->uri->as_iri =~ /\?(.*)/ )
+		{
+		    foreach( split(/&/, $1) )
+			{  my ($k,$v)=split(/=/,$_,2);
+			   $arg->{$k}=$v;
+			}
+		}
+	    my $ro;
+	    $ro->{"request"}=$r;
+	    $ro->{"args"}=$arg;
+	foreach my $g (  @pages ) {
+	    if (  $r->uri->path =~ /^$g->{p}$/ ) {
+		$ENV{'PATH_INFO'}=$1;
+		$ro->{"g"}=$g;
+		$ro->{"c"}=$c;
+		my $rv=$g->{"cb"}($ro);
+		_http_response($c, { content_type => 'text/html', charset=>'utf-8' },$rv)
+		    if $rv;
+		$matches++;
+		last;
+	    }
+	}
+	$c->send_error(RC_FORBIDDEN)
+	    unless $matches;
+
+        $c->close();
+        undef $c;
+    }
+}
+
+sub _http_error {
+    my ($c, $code, $msg) = @_;
+
+    $c->send_error($code, $msg);
+}
+
+sub _http_response {
+    my $c = shift;
+    my $options = shift;
+
+    $c->send_response(
+        HTTP::Response->new(
+            RC_OK,
+            undef,
+            [
+                'Content-Type' => $options->{content_type} . " charset=utf-8",
+		'charset' => 'utf-8',
+                'Cache-Control' => 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0',
+                'Pragma' => 'no-cache',
+                'Expires' => 'Thu, 01 Dec 1994 16:00:00 GMT',
+            ],
+            join("\n", @_),
+        )
+    );
+}
+
+sub get_pg
+{
+ return (
+ { p => '/p0', cb => sub { return "HiHo" } },
+ { p => '/', cb => sub { my $c=shift; $c->{"c"}->send_file_response("index.html"); return undef; }},
+ { p => '/docs(/.*)', cb => 
+	sub { 
+		my $c=shift; 
+		print "feed.cgi $1\n"; 
+		my $r0=qx{perl feed.cgi "$1"};
+		my $r=HTTP::Message->parse( $r0 );
+		print "L:".length($r->content)."\n";
+		my $rp=HTTP::Response->new(RC_OK,undef,$r->headers,$r->content);
+		$c->{"c"}->send_response( $rp );
+		return undef; 
+	}},
+ { p => '/ldres.cgi', cb => 
+	sub { 
+		my $c=shift; 
+		my $r=shift;
+		my $a=$c->{"args"};
+		print Dumper($c); # ->get_request(1));
+		return ldres($a->{"class"},$a->{"idx"},$a->{"ppages"},$a->{"search"});
+		return qx(perl ldres.cgi); 
+		return undef; 
+	}},
+ #{ p => '/res/ldres.cgi(.*)', cb => sub { return Dumper(@_); }},
+ { p => '/(.*)', cb => sub { my $c=shift;
+	return "Failed" unless ( -f $1 );
+ 	$c->{"c"}->send_file_response($1);
+	return undef;
+	}}
+   
+);
+}
---
+++ ld_r.pm
@@ -0,0 +1,372 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Data::Dumper;
+use WWW::Authen::Simple;
+use doclib::pdfidx;
+use Cwd 'abs_path';
+use CGI;
+use POSIX;
+
+$ENV{"PATH"} .= ":/usr/pkg/bin";
+my $docsrv="ts2.nispuk.com";
+
+my $__meta_sel;
+my $q     = CGI->new;
+my $ncols = 2;
+my $entries=9;
+
+my $pdfidx = pdfidx->new();
+
+my $dbs    = ( stat("/var/db/pdf/doc_db.db") )[7] / 1e6 . " Mb";
+
+# print pages
+my $ANY       = "*ANY*";
+# my $json_text = $q->param('json_string');
+# my $perl_scalar;
+# if ($json_text) {
+    # my $json = JSON::PP->new->utf8;
+    # $perl_scalar = $json->decode($json_text);
+# }
+# my $class = $q->param("class") || $perl_scalar->{"class"} || undef;
+
+
+my $dh = $pdfidx->{"dh"};
+
+if (1) {
+	open(TRC,">>/tmp/db.trace");
+	sub trace_it
+	{
+	  my $r=shift;
+
+	   print TRC "DB: $r\n";
+	}
+
+	$dh->sqlite_trace( \&trace_it);
+}
+
+$dh->do(
+q{ create table if not exists cache_lst ( qidx integer primary key autoincrement,
+		query text unique, nresults integer, last_used integer )}
+);
+$dh->do(
+q{ create table if not exists cache_q ( qidx integer, idx integer, id integer, snippet text, unique(qidx,idx))}
+);
+
+$dh->do(q{
+	CREATE TRIGGER if not exists cache_del before delete on cache_lst begin delete 
+		from cache_q where cache_q.qidx = old.qidx ; 
+	end;
+	});
+
+my $s1 = q{select qidx from cache_lst where query = ?};
+my $s2 = q{insert or abort into cache_lst (query) values(?)};
+my $s3 = q{
+	create temporary table cache_q_tmp as  
+		select ? qidx, docid idx,snippet(text) snippet from text where text match ?
+	};
+my $s3_fin = q{
+	insert into cache_q ( qidx,idx,id,snippet ) select qidx,idx,rowid,snippet from cache_q_tmp
+	};
+
+# ldres();
+# exit(0);
+
+sub ldres
+{
+
+  my ($class,$idx0,$ppage,$search)=@_;
+
+  my ($hd,$res)=("","");
+# my $idx0 = ( $q->param("idx") || 1 );
+# my $ppage = ( $q->param("count") || 18 );
+# my $search = $q->param("search") || undef;
+# my $ppage = $q->param("ppage")|| $entries;
+undef $search if $search && $search =~ /^\s*$/;
+	$idx0=1 unless $idx0;
+	$ppage = $entries unless $ppage;
+
+$class =~ s/:\d+$// if $class;
+undef $class if defined($class) && $class eq $ANY;
+
+# $hd=$q->header( -charset => 'utf-8' ),    # , -cookie=> \@mycookies), ;
+
+my $idx = $dh->selectrow_array( $s1, undef, $search );
+my ($dmin,$dmax);
+
+if ( $search && $search =~ s/\s*daterange:\s*(\d\d\d\d-\d\d-\d\d)\s*\.\.\.\s*(\d\d\d\d-\d\d-\d\d\s*)//i )
+{
+    # Search restrict to date-range
+    $dmin=$1;
+    $dmax=$2;
+
+}
+
+if ( $search && ! $idx ) {
+
+    # create cached results table
+    $dh->do( $s2, undef, $search );
+    $idx = $dh->last_insert_id( undef, undef, undef, undef );
+    my $nres=$dh->do( $s3, undef, $idx, $search );
+    $dh->do($s3_fin);
+    $dh->do("update cache_lst set nresults=? where qidx=?",undef,$nres,$idx);
+}
+
+my $subsel = "";
+if ($class) {
+    $dh->do(q{drop table if exists cls});
+    $dh->do(q{create temporary table cls ( tagid integer primary key unique )});
+    my $sth = $dh->prepare(
+        q{insert into cls (tagid) select tagid from tagname where tagname=?});
+    foreach ( split( /\s*,\s*/, $class ) ) {
+        $sth->execute($_);
+    }
+    $dh->do(q{drop table if exists docids});
+    $dh->do(q{create temporary table docids as select distinct(idx) idx  from cls natural join tags});
+    $subsel = "docids natural join ";
+}
+$dh->do(qq{drop table if exists drange });
+my ( $classes, $ndata, $stm1 );
+if ($search) {
+
+    # get final reslist
+    $dh->do(q{drop table if exists resl});
+    my $rest =
+      qq{create temporary table resl as select * from $subsel cache_q where qidx = ?};
+
+    $dh->do( $rest, undef, $idx );
+	print TRC "Tm: $dmin ... $dmax\n";
+    if ( $dmin )
+    {
+       my $drest =
+	
+
+	print TRC "Delete timerangs\n";
+	$dh->do(qq{create temporary table subd as select distinct(idx) idx  from dates where date between ? and ?},undef,$dmin,$dmax);
+	$dh->do( qq{ delete from resl where idx not in ( select idx from subd ) });
+    }
+
+    $dh->do(qq{ create temporary table drange as select min(date) min,max(date) max from dates natural join cache_q where qidx = ?},undef,$idx);
+    # get list of classes
+    $classes =
+q{select tagname,count(*) from tags natural join tagname where idx in ( select idx from resl) group by 1 order by 1};
+
+    # get number of results
+    $ndata = qq{select count(*) from resl};
+
+    # get display list
+    $stm1 = qq{ select * from resl join metadata m using (idx) where m.tag = "mtime" order by cast(m.value as integer) desc limit ?,?};
+}
+else {
+    $classes =
+qq{ select tagname,count(*) from $subsel tags natural join tagname group by 1};
+    $ndata = qq{ select count(*) from $subsel hash };
+    # qq{ select idx,value snippet from $subsel metadata where tag="Content" limit ?,?  };
+    $stm1 =
+	qq{ select s.idx,s.value snippet from $subsel metadata s join metadata m using (idx) where s.tag="Content" and m.tag="mtime" order by cast(m.value as integer)  desc limit ?,?  };
+    $dh->do(qq{ create temporary table drange as select min(date),max(date) from dates });
+}
+
+my $dater = join( " ... ",$dh->selectrow_array ("select * from drange"));
+$classes = $dh->selectall_arrayref($classes);
+$ndata   = $dh->selectrow_array($ndata);
+$stm1    = $dh->prepare($stm1);
+
+$stm1->bind_param( 1, $idx0-1 );
+$stm1->bind_param( 2, $ppage );
+$stm1->execute();
+
+# unshift @$classes,[$ANY,$ndata];
+# $classes=[map{ join(':',@$_)} @$classes];
+$classes = [
+    map {
+        my $ts = $$_[1] / "$ndata.0";
+        my $bg = "";#( $ts < 0.02 ) ? "background: #bbb" : "";
+        $ts = int( $ts * 40 );
+	$ts=19 if $ts>19;
+	$ts=9  if $ts <9;
+        $_  = $q->button(
+            -name  => 'button_name',
+            -class => 'tagbox',
+            -style => "font-size: ${ts}px; $bg ",
+            -value => $$_[0]
+          )
+    } @$classes
+];
+
+my $out = load_results($stm1);
+
+$res= $q->div( { -class => "tick", -id => "nresults" }, $ndata ).
+	$q->div({ -class => "tick", -id => "idx"},$idx0 ).
+	"&nbsp;".
+	$q->div({ -class => "tick", -id => "dates"},$dater ).
+	$q->div({ -class => "tick", -id => "pageno"},int($idx0/$ppage)+1 ).
+	$q->div({ -class => "tick", -id => "query"},$search).
+    $q->div( { -class => "tick", -id => "pages" }, pages($q,$idx0,$ndata,$ppage) ) .
+    $q->div( { -id => "classes" }, join( "", @$classes ) ) ;
+
+$res .=  "<br>";
+$res .=  $q->div($q->ul({-id =>"X_results"},$q->li({-class=>"rbox"},$out)));
+$res .=  $q->end_html;
+
+return $hd.$res;
+}
+
+# print page jumper  bar
+sub pages {
+    my ( $q, $idx0, $ndata, $ppage ) = @_;
+    my @pgurl;
+    my $p0 =1;
+    my $pi   = int(($idx0-1)/$ppage) +$p0;
+    my $prev_p=$pi-1;
+    $prev_p=1 if $prev_p<1;
+    my $last_p = int($ndata/$ppage)+$p0;
+    my $next_p =$pi+1;
+    $next_p=$last_p if $next_p > $last_p;
+
+    my $lo = $pi - int($entries/2);
+    $lo = $p0 if $lo < 1;
+    my $hi = $lo + $entries;
+    $hi = $last_p if $hi > $last_p;
+
+    push @pgurl, $q->button( -class => 'pageno', -value => "<<", -id => 1);
+    push @pgurl, $q->button( -class => 'pageno', -value =>  "<", -id => ($prev_p-1)*$ppage+1);
+
+    foreach ( $lo .. $hi ) {
+        my $i=($_-1)*$ppage+1;
+        push @pgurl, $q->button( -class =>( $_==$pi ? 'this_page' : 'pageno'), -value => $_ , -id => $i);
+    }
+    push @pgurl, $q->button( -class => 'pageno', -value => ">", -id => ($next_p-1)*$ppage+1);
+    push @pgurl, $q->button( -class => 'pageno', -value => ">>", -id =>($last_p-1)*$ppage+1);
+    return join("",@pgurl);
+    return $q->table( $q->Tr( $q->td( \@pgurl ) ) );
+}
+
+sub get_meta {
+    my $tag = shift;
+    $__meta_sel = $dh->prepare(q{select * from metadata where idx=?})
+      unless $__meta_sel;
+    $__meta_sel->execute($tag);
+    my $r = $__meta_sel->fetchall_hashref("tag");
+    return $r;
+}
+
+sub get_cell {
+    my ($r)  = @_;
+    my $meta = get_meta( $r->{"idx"} );
+    my $md5  = $meta->{"hash"}->{"value"};
+
+    my $editor = "edit.cgi?send=";
+    my $qt     = "'";
+    $editor .= "$md5&type=lowres";
+    my $s = 0;
+    my $p = "1";
+    my $d = $r->{"date"} || "--";
+    if ( my $mpdf=$meta->{"pdfinfo"}->{"value"} )
+    {
+	    $s = $1 if $mpdf =~ /File size\s*<\/td><td>\s*(\d+)/;
+	    $p = $1 if $mpdf =~ /Pages\s*<\/td><td>\s*(\d+)\s*<\/td>/;
+	    $d = $1 if $mpdf =~ /CreationDate\s*<\/td><td>(.*?)<\/td>/;
+    }
+     $p=1 unless $p;
+    my $tags =
+"select tagname from hash natural join tags natural join tagname where md5=\"$md5\"";
+    $tags = $dh->selectall_hashref( $tags, 'tagname' );
+    $tags = join( ",", sort keys %$tags );
+    $tags = $q->p(
+        $q->input(
+            {
+                -name  => "tags",
+                -id    => "$md5",
+                -type  => "text",
+                -class => "tagbox",
+                -value => $tags
+            }
+        )
+    );
+
+    my $short_name = $meta->{"Docname"}->{"value"};
+    $short_name =~ s/^.*\///;
+    my $sshort_name = $short_name;
+    $short_name =~ s/#/%23/g;
+
+    # build various URLS
+    #my $pdf    = "docs/pdf/$md5/$short_name";
+    my $pdf    = "web/viewer.html?file=../docs/pdf/$md5/$short_name";
+    my $lowres = "docs/lowres/$md5/$short_name";
+    #my $ico    = qq{<img src='docs/ico/$md5/$short_name'};
+    my $ico    = $q->img({-class=>"thumb", -src=>"docs/ico/$md5/$short_name"});
+    my $tip = $r->{"snippet"} || "";
+    #$tip =~ s/(.{1024}) .*/$1/;
+    #$tip =~ s/(([^\n]*\n){10}).*/$1/;
+    $tip =~ s/'/&quot;/g;
+    $tip =~ s/\n/<br>/g;
+    $tip = qq{'$tip'};
+    # print STDERR "TIP:$tip\n";
+
+# my @a=stat($pdf); my $e= strftime("%Y-%b-%d %a  %H:%M ($a[7]) $_",localtime($a[10]));
+    # $meta->{PopFile}->{value} =~ s|http://$docsrv|$q->url(-base=>'1')|e
+	# if $meta->{PopFile};;
+    my $day = $d;
+    $day =~ s/\s+\d+:\d+:\d+\s+/ /;
+    # $d = $&;
+    $d =~ s/^\s*\S*\s+//;
+    $d =~ s/\s+\d+:\d+:\d+\s+/ /;
+
+    my $data = 
+            $q->div({-class=>"thumb"},$q->a(
+                {   -class=>"thumb",
+                    -href        => $pdf,
+		    -target     => "docpage",
+                    -onmouseover => "Tip($tip)",
+                    -onmouseout  => "UnTip()"
+                },
+                $ico
+            )).
+            $q->div({-class=>"descr"}, 
+		   $q->a(
+                { -href => $meta->{PopFile}->{value}, -target => "_popfile" },
+                $meta->{Class}->{value} )
+              . $q->br
+              . $q->a( { -class=>"doclink", -href => $pdf ,-target => "docpage" }, $sshort_name )
+              . $q->br
+              . $q->a({-class=>"dtags"},$tags).
+            (
+                ( ( $s / $p ) > 500000 )
+                ? $q->a( { -href => $lowres, -target => "docpage" },
+                    "&lt;Lowres&gt;" )
+                : ""
+              )
+              . $q->a( { -href => $editor, -target => "docpage" },
+                "&lt;Edit&gt;" )
+	      . "<br>$d"
+              . "<br> Pages: $p <br>$s");
+
+    return $q->td( $q->div({-class=>"rcell"},$data));
+
+}
+
+sub load_results {
+    my ($stmt_hdl) = @_;
+    my $t0 = 0;
+    my @outrow;
+    my @out;
+    while ( my $r = $stmt_hdl->fetchrow_hashref ) {
+	    if ( 0) {
+        if ( $r->{"date"} && $t0 ne $r->{"date"} ) {
+            push @out, join( "\n  ", splice(@outrow) );
+
+            push @out, $q->th( { -colspan => $ncols }, $q->hr, $r->{"date"} );
+            $t0 = $r->{"date"};
+        }
+        push @outrow, get_cell($r);
+        push @out, join( "\n  ", splice(@outrow) ) if scalar(@outrow) >= $ncols;
+	}else{
+		push @out, get_cell($r);
+	}
+    }
+    # push @out, join( "\n  ", splice(@outrow) );
+    return \@out;
+}
+
+1;
