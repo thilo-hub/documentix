94cf8b6db21b26ae82c492ea2d79e8b7a79cdb5e
Thilo Jeremias <thilo@nispuk.com>
2016-12-02 07:46:34 +0100

    Disable hover during scrolling

--- js/docidx.js
+++ js/docidx.js
@@ -275,3 +275,19 @@ $(function() {
     }
 });
 
+var body = document.body,
+    timer;
+
+window.addEventListener('scroll', function() {
+  clearTimeout(timer);
+  if(!body.classList.contains('disable-hover')) {
+    body.classList.add('disable-hover')
+  }
+  
+  timer = setTimeout(function(){
+    body.classList.remove('disable-hover')
+  },500);
+}, false);
+
+
+
