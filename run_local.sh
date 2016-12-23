fad04c818f38799c83fe5a773481730012c5df6e
thilo jeremias <thilo@nispuk.com>
2016-12-23 23:49:34 +0100

    Fix run_local.sh

--- run_local.sh
+++ run_local.sh
@@ -2,7 +2,7 @@
 
 INST="$(pwd -P)"
 
-export PERL5LIB=$INST/local
+export PERL5LIB=$INST/local/lib/Perl5
 export PATH=$INST/local/bin:$PATH
 export LD_LIBRARY_PATH=$INST/local/lib:$LD_LIBRARY_PATH
 
