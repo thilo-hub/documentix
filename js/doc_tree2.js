4c117b4e9b640ce25aa54174e0df3da55df77a49
thilo jeremias <thilo@nispuk.com>
2017-01-12 08:18:58 +0100

    Unfinished work to have context menu attached to tree items

--- js/doc_tree2.js
+++ js/doc_tree2.js
@@ -1,3 +1,5 @@
+var CLIPBOARD = "";
+var defaultNode="X";
 $(function() {
 var process_node = function (node) {
 	if ( node.is_dir == 0 ) {
@@ -30,27 +32,57 @@ var process_node = function (node) {
 	    });
 	}
     }
+var $tree1 = $('#tree1');
 
 //$(document).ready(function() { $("#config").hide(); });
 // $('#tree1').tree({data: data});
 $.getJSON(
     '/dlist1.cgi',
     function(data) {
-        $('#tree1').tree({
-            data: data
+        $tree1.tree({
+            data: data,
+             usecontextmenu: true,
+	      onCreateLi: function(node, $li) {
+            // Append a link to the jqtree-element div.
+            // The link has an url '#node-[id]' and a data property 'node-id'.
+            $li.find('.jqtree-element').append(
+                '<a href="#node-'+ node.id +'" class="edit" data-node-id="'+
+                node.id +'">edit</a>'
+            );
+	   }
         });
     }
 );
-$('#tree1').bind(
+$tree1.bind(
     'tree.dblclick',
     function(event) {
         // The clicked node is 'event.node'
         var node = event.node;
-	process_node(node);
+	//process_node(node);
 
     }
 );
-
-
-
+$tree1.bind(
+    'tree.contextmenu',
+    function(event) {
+        // The clicked node is 'event.node'
+        var node = event.node;
+	defaultNode=node;
+        event.preventDefault();
+	$(event.target).contextMenu();
+	//process_node(node);
+    }
+);
+node_op = function(a,b) {
+  console.dir(a+" "+b);
+};
+    $.contextMenu({
+            selector: '.hasmenu',
+            callback: function(key, options) {
+                var m = "clicked: " + key;
+                 console.dir(m);
+		},
+	    items: $.contextMenu.fromMenu($('#filemenu'))
+        });
 });
+
