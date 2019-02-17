096ce7c9e9d6ab8215e8ec4570efceac9479dbce
thilo jeremias <thilo@nispuk.com>
2019-02-17 13:20:02 +0100

    Updated docs

--- start_docker.sh
+++ start_docker.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 # adjust the variables below to suit your system
 
-# To access your local DOCUMENTS through http://localhost:8888
-# Documents will be found under the Documents folder below 
-# Work-space for uploaded and ocr'd documents is in incoming
-# New documents (drag&drop) will be stored in .../incoming/
-#use:
 
+# Read-only document folder
 DOCUMENTS=$PWD/Documents
+# Location for uploaded and processed documents
 INCOMING=$PWD/incoming
+# Main web interface
 PORT="127.0.0.1:28080"
+# Access popfile (not needed usually)
 PORT2="127.0.0.1:28081"
 
 test -d "$INCOMING" || mkdir "$INCOMING"
 
 echo "Documents: $DOCUMENTS"
 echo "Incoming: $INCOMING"
-# --entrypoint bash -ti \
+
 docker run -p $PORT:80/tcp  -p $PORT2:18080/tcp \
 	--rm --detach \
 	--mount type=bind,src=$DOCUMENTS,dst=/documentix/Documents/Documents,readonly \
