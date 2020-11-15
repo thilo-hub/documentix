eb4ef29162c3277a3860610779b07698c402182d
Thilo Jeremias <thilo@nispuk.com>
2020-11-15 15:26:20 +0100

    Add a test script
    running this script requires some test data which I haven't published.
    
    public test-data should be used but requires some manual classification first

--- tests/tests_1.sh
+++ tests/tests_1.sh
@@ -1,12 +1,22 @@
-PORT="http://localhost:65000"
+PORT="http://*:65000"
 ../script/documentix daemon -l $PORT &
 BG=$!
 sleep 4
+curl -v --data-binary '@data.tar.gz' -H "X-File-Name: testdata.tar.gz" http://localhost:65000/upload
+curl -v --data-binary '@data.zip' -H "X-File-Name: testdata.zip" http://localhost:65000/upload
 perl  -I../lib -e 'BEGIN{ $Documentix::config=require "./documentix.conf";}  use Documentix::classifier; pdf_class_md5();'
-perl  ../script/documentix get -M POST -f 'json_string={"op":"rem","md5":"0c294f6e80f1e5dfe27fbf74027154f3","tag":"WWW"}'  /tags
-perl ../script/documentix get /docs/ico/aa96e95cbe71de105e7d51388b334ad8/a%20new%20pdf.ico > a
+perl  ../script/documentix get -M POST -f 'json_string={"op":"rem","md5":"b5681751a87cdd5dc1b79c89791a822f","tag":"WWW"}'  /tags
+perl ../script/documentix get /docs/ico/b5681751a87cdd5dc1b79c89791a822f/a%20new%20pdf.ico > a
 
+#Import a directory
+tar xvfz Import.Dir.tar.gz -C Docs/.
+curl http://localhost:65000/refresh
+
+echo press enter to stop
+read F
 kill -INT $BG
 wait
  pkill -f 'perl ../script/documenti'
  pkill -f popfile
+
+echo Finish
