3f9c386f9c18530baa85c66cfbc5c164206d05ad
Thilo Jeremias <thilo@nispuk.com>
2020-12-01 22:23:00 +0100

    Missed to convert searching

--- lib/Documentix/search.pm
+++ lib/Documentix/search.pm
@@ -39,15 +39,15 @@ sub search {
     # here we simply reject such queries
     # Check if we could escape it...
     return undef if $search =~ m/:/; 
-    $dh->do("begin transaction");
-    my $n = $dh->do( $cache_setup, undef, $search );
-    my $idx = $dh->selectrow_array( $cache_lookup, undef, $search );
+    dh->do("begin transaction");
+    my $n = dh->do( $cache_setup, undef, $search );
+    my $idx = dh->selectrow_array( $cache_lookup, undef, $search );
     if ( $n != 0  ) {
 	    # we have a new search
 
 	    # Arguments for search query
 	    my @sargs = ( $idx, $search );
-	    my $srch = $dh->prepare_cached($cached_search);
+	    my $srch = dh->prepare_cached($cached_search);
 
 	    # if a date-range is mentioned, fix the search sql to select the time range only
 	    my $date_match = '\s*(\d\d\d\d-\d\d-\d\d)\s*\.\.\.\s*(\d\d\d\d-\d\d-\d\d)(\s*|$)';
@@ -57,11 +57,11 @@ sub search {
 		# remove range from search string and process normally
 		# Search restrict to date-range ( will reduce output list )
 		@sargs = ( $idx, $1, $2 );
-		$srch = $dh->prepare_cached($search_date);
+		$srch = dh->prepare_cached($search_date);
 		unless ( $search =~ /^\s*$/ ) {
 		    # date with text match
 		    push @sargs, $search;
-		    $srch = $dh->prepare_cached($search_date_txt);
+		    $srch = dh->prepare_cached($search_date_txt);
 		}
 	    }
 
@@ -70,15 +70,15 @@ sub search {
 	    print STDERR "A:" . join( ":", @sargs ) . ":\n";
 	    my $nres = $srch->execute(@sargs);
 	    print STDERR "R: $nres\n";
-	    $dh->do(q{delete from cache_lst where qidx=?},undef,$idx)
+	    dh->do(q{delete from cache_lst where qidx=?},undef,$idx)
 		    if ( $srch->err );
 
 
 	    # record search results
-	    $dh->prepare_cached( 'update cache_lst set nresults=?,last_used=datetime("now")  where qidx=?')
+	    dh->prepare_cached( 'update cache_lst set nresults=?,last_used=datetime("now")  where qidx=?')
 		->execute( $nres, $idx );
 	}
-    $dh->do("commit");
+    dh->do("commit");
     return $idx;
 }
 
