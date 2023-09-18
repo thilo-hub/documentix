1dc294875e7b21d9f77071a366473a228f6a8a1e
Thilo <thilo@nispuk.com>
2023-09-19 00:21:42 +0200

    update build script

--- build_local.sh
+++ build_local.sh
@@ -7,20 +7,22 @@ test -d $DEST || mkdir $DEST
 SQLSRC="https://www.sqlite.org/2023/sqlite-amalgamation-3430100.zip"
 
 
-cpan -g DBD::SQLite 
-test -r DBD-SQLite-*.tar.gz &&
-	tar xvfz DBD-SQLite*.tar.gz && rm -f DBD-SQLite*.tar.gz &&
-	cd DBD-SQLite* &&
-	wget "$SQLSRC"  &&  
-	unzip -j -o  "$(basename "$SQLSRC")" &&
-	perl Makefile.PL  && 
-	make && 
-	make DESTDIR=$DEST  install &&
-	git clone --recursive --depth 1 https://github.com/abiliojr/fts5-snowball.git &&
-	cd fts5-snowball/ &&  
-	make SQLITE_FLAGS="-I$PWD/.." && 
-	mkdir -p $DEST/usr/lib  && 
-	cp fts5stemmer.so $DEST/usr/lib/.
+echo NO cpan -g DBD::SQLite 
+echo NO test -r DBD-SQLite-*.tar.gz &&
+	echo NO tar xvfz DBD-SQLite*.tar.gz && rm -f DBD-SQLite*.tar.gz &&
+	echo NO cd DBD-SQLite* &&
+	echo NO wget "$SQLSRC"  &&  
+	echo NO unzip -j -o  "$(basename "$SQLSRC")" &&
+	echo NO perl Makefile.PL  && 
+	echo NO make && 
+	echo NO make DESTDIR=$DEST  install &&
+        git submodule init   &&
+	git submodule update --init --recursive  &&
+	cd fts5-snowball/ &&
+	gmake SQLITE_FLAGS="-I/usr/local/include" && 
+	sudo cp fts5stemmer.so $DEST/usr/local/lib/.
+	cd .. &&
+	git submodule deinit --all
 
 
 
