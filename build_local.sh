e420ffc45ec28a3606a85dffec0ab392919ffe37
thilo jeremias <thilo@nispuk.com>
2023-10-09 00:56:46 +0200

    Slimmer build local stuff

--- build_local.sh
+++ build_local.sh
@@ -17,29 +17,16 @@
 MAKE=gmake
 MAKE=make
 
-INC="/usr/local/include"
-test -d /root/.cpan/build/DBD-SQLite-1.74-0/ && INC=/root/.cpan/build/DBD-SQLite-1.74-0
 DEST="$1";
-test -d $DEST || mkdir $DEST
-SQLSRC="https://www.sqlite.org/2023/sqlite-amalgamation-3430100.zip"
+INC="$(dirname "$(find / -name sqlite3ext.h | tail -1)")"
 
-
-echo NO cpan -g DBD::SQLite 
-echo NO test -r DBD-SQLite-*.tar.gz &&
-	echo NO tar xvfz DBD-SQLite*.tar.gz && rm -f DBD-SQLite*.tar.gz &&
-	echo NO cd DBD-SQLite* &&
-	echo NO wget "$SQLSRC"  &&  
-	echo NO unzip -j -o  "$(basename "$SQLSRC")" &&
-	echo NO perl Makefile.PL  && 
-	echo NO make && 
-	echo NO make DESTDIR=$DEST  install &&
-        git submodule init   &&
-	git submodule update --init --recursive  &&
-	cd fts5-snowball/ &&
-	$MAKE SQLITE_FLAGS="-I$INC" && 
-	cp fts5stemmer.so $DEST/usr/local/lib/.
-	cd .. &&
-	git submodule deinit --all
+git submodule init   &&
+git submodule update --init --recursive  &&
+cd fts5-snowball/ &&
+$MAKE SQLITE_FLAGS="-I$INC" && 
+install -t $DEST/lib fts5stemmer.so &&
+cd .. &&
+git submodule deinit --all
 
 
 
