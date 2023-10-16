40d797e413d9951edb8c668569231d037c3dd26e
Thilo Jeremias <thilo@nispuk.com>
2023-10-17 00:32:41 +0200

    Update database scheme
    not inserting into cache_lst create the searches db

--- lib/Documentix/dbaccess.pm
+++ lib/Documentix/dbaccess.pm
@@ -14,7 +14,7 @@ use Digest::MD5 qw(md5 md5_hex md5_base64);
 use Archive::Zip qw( :ERROR_CODES :CONSTANTS );
 use Encode qw{encode decode};
 
-my $dbversion = "1";
+my $dbversion = "6";
 
 my $jobid=$$;
 my $debug = 2;
@@ -401,6 +401,73 @@ sub dbupgrade
 		$ins->execute($r->{idx}+0,"Content",encode("UTF-8",$c));
 	    }
     }
+    if ( $oldversion < 6) {
+	my @ops = (
+		qq{ drop TRIGGER if exists results_fill;},
+		qq{ drop TRIGGER if exists new_search;},
+		qq{ drop TRIGGER if exists cache_fetch;},
+		qq{ drop TRIGGER if exists cache_fill;},
+		qq{ drop TRIGGER if exists cache_new;},
+		qq{ drop TRIGGER if exists cache_hit;},
+		qq{ drop TRIGGER if exists cache_fill_h;},
+		qq{ drop TRIGGER if exists cache_fill_t;},
+		qq{ drop view if exists  cache_q_stat;},
+		qq{ drop view if exists  mylog_cache_lst;},
+		qq{ drop table if exists  cache_lst;},
+		qq{ drop table if exists  cache_q;},
+		## Update search to be trigger only
+		#TODO: Cahnge search to use it
+		qq{ CREATE TABLE cache_q ( qidx integer, idx integer, snippet text, rank float, primary key (qidx,idx));},
+		qq{ CREATE TABLE if not exists cache_lst ( qidx integer primary key autoincrement,
+					      query text unique, nresults integer, hits integer, last_used integer DEFAULT (unixepoch()));},
+	        qq{ CREATE TRIGGER if not exists cache_del before delete on cache_lst begin delete from cache_q where cache_q.qidx = old.qidx ; end;
+		},
+			##  debug
+		qq{ CREATE TABLE if not exists mylog (idx,md5,refcnt,time default CURRENT_TIMESTAMP);},
+		qq{ create view if not exists mylog_cache_lst as
+			select mylog.* from cache_lst join mylog on(idx='q'||qidx) order by mylog.rowid;
+		},
+			##  list actual cache contents
+		qq{ create view cache_q_stat(qidx,hits,nresults) as select qidx,count(*),sum(iif(snippet is null,0,1)) from cache_q group by qidx; },
+			## a) insert new search and request no results (NULL)
+			## b) insert new search and request n results (n>=0)
+			## c) update search and request n results (n>=0)
+
+			##  Disable/enable logging for debugging by commenting the mylog
+		qq{ CREATE TRIGGER cache_new after insert on cache_lst when new.nresults is not null begin
+			insert into mylog(idx,md5,refcnt) values('q'||new.qidx,'cache_new: ' || new.nresults,0);
+			update cache_lst set hits = -1  where qidx = new.qidx;
+		end;},
+		##  setting the hit to -1 rebuilds the matched document cache
+		qq{ CREATE TRIGGER cache_hit  after update of hits on cache_lst when  not new.hits >= 0 begin
+			insert into mylog(idx,md5) values('q'||new.qidx,'cache_hits '||ifnull(old.hits,"NULL")|| ' -> ' || new.hits);
+			insert or replace into cache_q(qidx,idx,rank) select new.qidx,docid,rank from text where text match new.query;
+			update cache_lst set hits=hit,nresults = -1  from (select nresults nr,hits hit from cache_q_stat where qidx=new.qidx) where new.qidx = qidx;
+		end;
+		},
+		##  setting the nresults to -1 reloads previous number of results ( or initially loads the results)
+		##  increasing  nresults fetches more snippets from documents
+		qq{ CREATE TRIGGER cache_fill after update of nresults on cache_lst when new.nresults < 0 or (new.nresults > old.nresults and new.hits > old.nresults) begin
+			insert into mylog(idx,md5) values('q'||new.qidx,'cache_fill: '||ifnull(old.nresults,"NULL")|| ' -> ' || new.nresults);
+			update cache_q set snippet=snip2 from (
+				select idx idx2,snippet(text,1,'<b>','</>','...',5) snip2
+				from (select qidx,idx from cache_q where qidx=new.qidx and snippet is null
+							   order by rank
+							   limit iif(new.nresults < 0,old.nresults,new.nresults-old.nresults))  join text on(docid=idx)
+				where text match new.query
+				) where qidx=new.qidx and idx2 = idx;
+			update cache_lst set nresults=nr from (select nresults nr from cache_q_stat where qidx=new.qidx) where new.qidx = qidx;
+		end;
+
+		}
+
+	);
+	foreach(@ops) {
+		print STDERR "EX: $_\n";
+		 $self->{dh}->do($_);
+	}
+    }
+
     $self->dbmaintenance1();
     $dh->do(q{insert or replace into config (var,value) values("dbversion",?)},undef,$dbversion);
 }
