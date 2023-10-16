3386e5c0004f80cb79418f68168633caf301b719
Thilo Jeremias <thilo@nispuk.com>
2023-10-17 00:36:14 +0200

    New search wat
    insert on db will creaate the first 100 results
    after that results wiil e generate on demand
    "date:1900-01-01...2027-01-01
    sytax only works when cache is loaded....

--- lib/Documentix/search.pm
+++ lib/Documentix/search.pm
@@ -1,37 +1,29 @@
 package Documentix::search;
 
 use Exporter 'import'; 
-our @EXPORT = qw{search};
+our @EXPORT = qw{fts_search fts_loadmore};
 
 use Documentix::db qw{dh};
 
 
-print STDERR dh,"\n";
+# print STDERR dh,"\n";
 
-#input 
-# search-string
+
+
 #
-# output:
-# w
-my $cache_lookup = q{select qidx from cache_lst where query like ?};
-my $cache_setup  = q{insert or ignore into cache_lst (query) values(?)};
-
-my $cached_search = q{insert or ignore into cache_q ( qidx,idx,snippet ) 
-                                select ?,docid,snippet(text,1,"<b>","</b>","...",10) 
-					from text  join hash on (docid=idx) where text match ? order by rank limit 500  };
-# Extend search to include date range
-my $search_date_txt = $cached_search;
-   $search_date_txt =~ s/where/natural join dates where date between ? and ? and/;
+my $cache_lookup = q{select qidx,nresults,hits from cache_lst where query like ?};
+my $cache_setup  = q{insert or ignore into cache_lst (query,nresults) values(?,?)};
 
 # special case only search for dates ( no matching )
-my $search_date = "insert or ignore into cache_q (qidx, idx, snippet) select ?,idx,mtext
-			from dates where date between ? and ?";
 
-my $date_match = qr{date[:\s*](\d\d\d\d-\d\d-\d\d)\s*\.\.\.\s*(\d\d\d\d-\d\d-\d\d)(\s*|$)};
+sub  fts_loadmore{
+	my ( $qidx, $nresults) = @_;
+	my $q = dh->prepare_cached(qq{update cache_lst set nresults=? where qidx=?});
+	$q->execute($nresults,$qidx);
+}
 
-
-sub search {
-    my ( $search ) = @_;
+sub fts_search {
+    my ( $search,$slimit ) = @_;
 
     return undef unless $search;
    
@@ -40,46 +32,23 @@ sub search {
     # here we simply reject such queries
     # Check if we could escape it...
     dh->do("begin transaction");
-    my $n = dh->do( $cache_setup, undef, $search );
-    my $idx = dh->selectrow_array( $cache_lookup, undef, $search );
-    if ( $n != 0  ) {
-	    # we have a new search
 
-	    # Arguments for search query
-	    my @sargs = ( $idx, $search );
-	    my $srch = dh->prepare_cached($cached_search);
-    $DB::single=1;
-
-	    # if a date-range is mentioned, fix the search sql to select the time range only
-	    if ( $search && $search =~ s/$date_match//i ) {
-		print STDERR "Datesearch:  $1 -- $2\n";
-		# daterange specified...
-		# remove range from search string and process normally
-		# Search restrict to date-range ( will reduce output list )
-		@sargs = ( $idx, $1, $2 );
-		$srch = dh->prepare_cached($search_date);
-		unless ( $search =~ /^\s*$/ ) {
-		    $search =~ s/:/ /gs; # fts sees arguments ... unhappy
-		    # date with text match
-		    push @sargs, $search;
-		    $srch = dh->prepare_cached($search_date_txt);
-		}
-	    }
-
-	    # Do search
-	    print STDERR "S:$cached_search\n";
-	    print STDERR "A:" . join( ":", @sargs ) . ":\n";
-	    my $nres = $srch->execute(@sargs);
-	    print STDERR "R: $nres\n";
-	    dh->do(q{delete from cache_lst where qidx=?},undef,$idx)
-		    if ( $srch->err );
+    # Clean search from fts syntax.. unless quoted
+    unless ( $search =~ s|^QUOTE:|| ) {
+	$search =~ s/[(){}\.\;,\$]/ /gs;
+	$search = "NEAR($search ,100)";
+    }
 
 
-	    # record search results
-	    dh->prepare_cached( 'update cache_lst set nresults=?,last_used=datetime("now")  where qidx=?')
-		->execute( $nres, $idx );
-	}
+    # $DB::single=1;
+    my $n = dh->do( $cache_setup, undef, $search, $slimit || 100 );
+    my $idx = dh->last_insert_id();  
     dh->do("commit");
-    return $idx;
+    my @idx = dh->selectrow_array( $cache_lookup, undef, $search ) ;
+    return @idx;
+
+    # TODO: read acces does not trigger time-stamp...
+    dh->prepare_cached( 'update cache_lst set last_used=datetime("now") where qidx=?')
+		->execute( $idx );
 }
 
